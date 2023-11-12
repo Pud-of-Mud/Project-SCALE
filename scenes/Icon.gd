db8cb1b259eac319bc1f74e254b2eb77aef4ed73
Pud-of-Mud <109466200+Pud-of-Mud@users.noreply.github.com>
2023-11-11 21:43:02 -0500

    Added functioning 2.5D Scene
    Also w/ Movement & Jump; still needs fine tuning for optimum movement.

--- scenes/Icon.gd
+++ scenes/Icon.gd
@@ -1,6 +1,7 @@
 extends Sprite
 
 # Declare member variables here.
+var movementSpeed = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,16 +9,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	# When the key is pressed, than it translates the Node in the desidered direction
 	# NOTE: Uses TRANSLATION; NOT anything physics based.
 	if Input.is_key_pressed(KEY_W):
-		translate(Vector2(0,-1))
+		translate(Vector2(0,-movementSpeed))
 	
 	if Input.is_key_pressed(KEY_A):
-		translate(Vector2(-1,0))
+		translate(Vector2(-movementSpeed,0))
 	
 	if Input.is_key_pressed(KEY_S):
-		translate(Vector2(0,1))
+		translate(Vector2(0,movementSpeed))
 	
 	if Input.is_key_pressed(KEY_D):
-		translate(Vector2(1,0))
+		translate(Vector2(movementSpeed,0))
---
+++ scenes/KinematicBodyGravity.gd
@@ -0,0 +1,31 @@
+extends KinematicBody
+
+# Defining the physics
+var velocity = Vector3(0, 0, 0) # Needs to be vector3 becasue of using the 3D Engine. 
+var gravity = 10.0
+var jumpHeight = 9.0
+var movementSpeed = 7.0
+
+
+func _physics_process(delta):
+	# Reset the velocity every frame to prevent "stacking"
+	# of left and right movement.
+	velocity = Vector3(0, velocity.y, 0)
+
+	# Apply gravity
+	if not(is_on_floor()):
+		velocity.y -= gravity * delta
+	
+	# Inputs & Physics for movement 
+	if Input.is_key_pressed(KEY_D):
+		velocity.x += movementSpeed
+	if Input.is_key_pressed(KEY_A):
+		velocity.x -= movementSpeed
+
+	# Input & Physics for jumping
+	if Input.is_key_pressed(KEY_SPACE) and is_on_floor():
+		velocity.y = jumpHeight
+
+	
+	# Move the KinematicBody
+	velocity = move_and_slide(velocity, Vector3.UP)
